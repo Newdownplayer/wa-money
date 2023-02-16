13d09e6e0156b6328cfe487c931d3dc19e55ca5e
name <1119313277@qq.com>
2023-02-16 15:13:06 +0800

    创建三个页面

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    app
+    <hr>
     <router-view/>
-  </div>
+    <hr>
+      <div class="router">
+        <router-link to="/money">记账</router-link>
+        |
+        <router-link to="/labels">标签</router-link>
+        |
+        <router-link to="/statistics">统计</router-link>
+      </div>
+    </div>
 </template>
 
 <style lang="scss">
